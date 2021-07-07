3674be08efabc49883653dcf2676adafca5e2429
Maria Pritchina <deco-bots@yandex.ru>
2021-07-07 14:45:03 +0300

    Update telegram-bot.py

--- telegram-bot.py
+++ telegram-bot.py
@@ -3,10 +3,27 @@ from engine import get_random_quote
 import os
 
 
+from __future__ import print_function
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+
 # Your bot token (from BotFather)
 TOKEN = os.environ.get("ACCESS_TOKEN")
 PORT = int(os.environ.get('PORT', 5000))
-GOOGLE_ID = os.environ.get("GOOGLE_ID")
+
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/documents.readonly']
+
+# The ID of a sample document.
+DOCUMENT_ID = os.environ.get("GOOGLE_ID")
+
+
+
+
 
 
 def start(bot, update):
@@ -19,6 +36,38 @@ def quote(bot, update):
 					text=get_random_quote())
 
 def main():
+	 """Shows basic usage of the Docs API.
+    Prints the title of a sample document.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('docs', 'v1', credentials=creds)
+
+    # Retrieve the documents contents from the Docs service.
+    document = service.documents().get(documentId=DOCUMENT_ID).execute()
+
+    print('The title of the document is: {}'.format(document.get('title')))
+
+
+	
+	
+	
 	updater = Updater(TOKEN);
 	dp = updater.dispatcher
 
