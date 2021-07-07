ff66ac58a0e728e39d84af48fdbcd78048b0b539
Maria Pritchina <deco-bots@yandex.ru>
2021-07-07 18:30:51 +0300

    Update telegram-bot.py

--- telegram-bot.py
+++ telegram-bot.py
@@ -1,62 +1,23 @@
-import google.oauth2.credentials
-import google_auth_oauthlib.flow
 
 
 from telegram.ext import Updater, CommandHandler
 from engine import get_random_quote
 import os
 
-from googleapiclient.discovery import build
-
-
 
 # Your bot token (from BotFather)
 TOKEN = os.environ.get("ACCESS_TOKEN")
 PORT = int(os.environ.get('PORT', 5000))
-GOOGLE_AUTORISATION_TOKEN = os.environ.get("GOOGLE_AUTORISATION_TOKEN")
-
-
-
-# If modifying these scopes, delete the file token.json.
-SCOPES = ['https://www.googleapis.com/auth/documents.readonly']
-
-# The ID of a sample document.
-DOCUMENT_ID = os.environ.get("GOOGLE_ID")
-
-
-
 
 
 
 def start(bot, update):
-	bot.sendMessage(chat_id=update.message.chat_id, text=("привет %s. Набирай /foilar и лови мудрость!"
-														  "me!" %
-														  update.message.from_user.name))
+	bot.sendMessage(chat_id=update.message.chat_id, text=("привет %s. Набирай /foilar и лови мудрость!" "me!" % update.message.from_user.name))
 
 def quote(bot, update):
-	bot.sendMessage(chat_id=update.message.chat_id,
-					text=get_random_quote())
+	bot.sendMessage(chat_id=update.message.chat_id, text=get_random_quote())
 
 def main():
-	#flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file('credentials.json', scopes=SCOPES)
-	#flow.redirect_uri = 'http://localhost:35655/'
-	#flow.fetch_token(code=GOOGLE_AUTORISATION_TOKEN)
-	flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
-	'credentials.json',
-	scopes=SCOPES)
-	flow.redirect_uri = 'http://localhost:35655/'
-	creds = flow.run_local_server(port=0)
-
-	print(flow)
-	# Create an httplib2.Http object to handle our HTTP requests and authorize it
-	# with our good Credentials.
-	with build('documentai', 'v1', creds) as service:
-		doc = service.documents.get(DOCUMENT_ID).execute()
-		
-		
-	
-	
-	
 	updater = Updater(TOKEN);
 	dp = updater.dispatcher
 
