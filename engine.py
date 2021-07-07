703d97edf58c406779c7c10278c3cabb147d7b12
Maria Pritchina <deco-bots@yandex.ru>
2021-07-07 18:34:50 +0300

    Update engine.py

--- engine.py
+++ engine.py
@@ -9,25 +9,26 @@ def get_random_quote():
     # Open the quote file
     with open(quote_file) as file:
         line = file.readlines()
+        random_line = (randint(0, len(line)-1))
 
     # Let's begin with some random line number
     # When '%%' is found, save the line number and break the loop
-    for i in range(len(line)-1):
-        random_line = (randint(0, len(line)-1))
-        if "%%" in line[random_line]:
-            start_line = random_line
-            break
+    # for i in range(len(line)-1):
+     #    random_line = (randint(0, len(line)-1))
+     #    if "%%" in line[random_line]:
+     #        start_line = random_line
+      #       break
 
     # Find the closest next '%%' line number
-    for i in range(start_line+1, len(line)):
-        if "%%" in line[i]:
-            end_line = i
-            break
+    # for i in range(start_line+1, len(line)):
+      #   if "%%" in line[i]:
+     #        end_line = i
+     #        break
 
     # We don't need the '%%' to be printed
-    start_line += 1
+   #  start_line += 1
 
     # Join all the text between these two '%%'
-    quote = "".join(line[start_line:end_line])
+   #  quote = "".join(line[start_line:end_line])
 
-    return quote
+    return random_line
